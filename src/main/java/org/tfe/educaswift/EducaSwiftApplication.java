12361b47ca50810c19d84549683539dcf015e2ca
Kaue Miziara <kauemiziara@gmail.com>
2023-12-04 10:05:45 -0300

    Initial commit

---
+++ src/main/java/org/tfe/educaswift/EducaSwiftApplication.java
@@ -0,0 +1,13 @@
+package org.tfe.educaswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducaSwiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducaSwiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tfe/educaswift/EducaSwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tfe.educaswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaSwiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
